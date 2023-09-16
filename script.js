24b825d8433b99e1e3f3eea047441aa02bab1ec7
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-16 13:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
+  //Write your code here
+let fname = document.querySelector("[name=fname]").value;
+let lname = document.querySelector("[name=lname]").value;
+window.alert(fname+" "+lname);
 
 }
